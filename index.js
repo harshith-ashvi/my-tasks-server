80c30bcbc6adf162b00ba5539c9a12c7d616d54b
harshith <harshith.ashvi@gmail.com>
2022-01-09 11:41:48 +0530

    Added CRUD Operations

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const app = express()
+app.use(express.json())
 const mongoose = require("mongoose")
 const port = 3033
 
@@ -45,6 +46,66 @@ app.get("/", (req, res) => {
     res.send("Welcome to My Tasks website")
 })
 
+//Get all tasks
+app.get("/api/tasks", (req, res) => {
+    Task.find()
+        .then((tasks) => {
+            res.json(tasks)
+        })
+        .catch((err) => {
+            res.json(err)
+        })
+})
+
+//Create a task
+app.post("/api/tasks", (req, res) => {
+    const body = req.body
+    const task = new Task(body)
+    task.save()
+        .then((task) => {
+            res.json(task)
+        })
+        .catch((err) => {
+            res.json(err)
+        })
+})
+
+//Find one task
+app.get("/api/tasks/:id", (req, res) => {
+    const id = req.params.id
+    Task.findById(id)
+        .then((task) => {
+            res.json(task)
+        })
+        .catch((err) => {
+            res.json(err)
+        })
+})
+
+//Update task
+app.put("/api/tasks/:id", (req, res) => {
+    const id = req.params.id
+    const body = req.body
+    Task.findByIdAndUpdate(id, body, { new: true, runValidators: true })
+        .then((task) => {
+            res.json(task)
+        })
+        .catch((err) => {
+            res.json(err)
+        })
+})
+
+//Delete Operation
+app.delete("/api/tasks/:id", (req, res) => {
+    const id = req.params.id
+    Task.findByIdAndDelete(id)
+        .then((task) => {
+            res.json(task)
+        })
+        .catch((err) => {
+            res.json(err)
+        })
+})
 
 
 
